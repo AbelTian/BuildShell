a58aac9dbea575cb9ff204dafca044c51b408f0d
tianduanrui <2407223896@qq.com>
2017-10-14 20:45:38 +0800

    update example

--- example.sh
+++ example.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+function env_effect(){
+    mm export $1 $2
+    source $(mm source root)/$2_effect.sh
+}
 
+function env_unset(){
+    source $(mm source root)/$1_unset.sh
+}
 
-
-#env effect
+#env effect (shared environment)
 mm set cur env 'qt.android'
 #do command in this env
 mm exec qqt.build
@@ -11,25 +17,35 @@ mm exec qqt.build
 
 
 
-#env effect
+#env effect (shared environment)
 mm set cur env 'android.x86'
-#do command in this env
+#do configed command in this env
 mm exec qqt.build
 #env reset (no need)
 
 
-
-#env effect
-mm set cur env 'android.mobile'
-#do command in this env
-mm exec qqt.build
-#env reset (no need)
-
-
+#env effect (private environment)
+env_effect 'android.x86' x86
+#do custom command in this env
+java -version
+env_unset x86
+#env reset (need)
 
 
-#env effect
+#env effect (shared environment)
 mm set cur env 'android.x86'
+
+
+#env effect (private environment)
+env_effect 'android.mobile' mobile
+#do custom command in this env
+env_unset mobile
+
+
+
+
+
+#env effect (shared environment)
 mm export
 source $(mm source root)/env_effect.sh
 #do command in this env
@@ -39,7 +55,7 @@ source $(mm source root)/env_unset.sh
 
 
 
-#env effect
+#env effect (shared environment)
 mm set cur env 'qt.android'
 #do command in this env
 mm exec 'java -version'
