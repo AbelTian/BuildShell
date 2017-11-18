d6dbee720c89d891b1336ee4183e1a85642ce6ae
tianduanrui <2407223896@qq.com>
2017-11-18 11:36:58 +0800

    add code counter

--- build.qqt.sh
+++ build.qqt.sh
@@ -4,7 +4,7 @@
 #env effect
 mm set cur env 'macOS'
 #do command in this env
-mm exec qqt.build
+mm exec build.qqt
 #env reset (no need)
 
 
@@ -17,12 +17,12 @@ source $(mm source root)/qt4_effect.sh
 source $(mm source root)/qt4_unset.sh
 
 mm set cur env qt4
-mm exec qqt.build
+mm exec build.qqt
 
 #env effect
 mm set cur env 'android.mobile'
 #do command in this env
-mm exec qqt.build
+mm exec build.qqt
 #env reset (no need)
 
 
@@ -30,10 +30,13 @@ mm exec qqt.build
 #env effect
 mm set cur env 'android.x86'
 #do command in this env
-mm exec qqt.build
+mm exec build.qqt
 #env reset (no need)
 
+mm set cur env iOS
+mm exec build.qqt
+
 mm set cur env iOSSimulator
-mm exec qqt.build
+mm exec build.qqt
 
 mm set cur env macOS
